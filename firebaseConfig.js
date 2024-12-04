1aa0f93e03fd50df80a891a5b4eea8a490c1bfdb
Jorge Edgar Rojas Magdaleno <58581611+iserm-git@users.noreply.github.com>
2024-12-04 09:51:23 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANd0kgDy6xXwazNxnkhos_GKCN9spVW08",
-  authDomain: "appescuela-ea8c1.firebaseapp.com",
-  projectId: "appescuela-ea8c1",
-  storageBucket: "appescuela-ea8c1.firebasestorage.app",
-  messagingSenderId: "926127922641",
-  appId: "1:926127922641:web:a8e5c81a93fef14f6554f0",
+  apiKey: "CAMBIAR",
+  authDomain: "CAMBIAR",
+  projectId: "CAMBIAR",
+  storageBucket: "CAMBIAR",
+  messagingSenderId: "CAMBIAR",
+  appId: "1:CAMBIAR",
 };
 
 const app = initializeApp(firebaseConfig);
